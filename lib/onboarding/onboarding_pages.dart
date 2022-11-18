df982074a5ffbcfa89a12f625a8211a2560f253a
jawadaljarrash <jawad_1422@hotmail.com>
2022-11-19 02:14:34 +0300

    onboarding_pages

--- lib/onboarding/onboarding_pages.dart
+++ lib/onboarding/onboarding_pages.dart
@@ -3,25 +3,140 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:introduction_screen/introduction_screen.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class OnBoardingPage extends StatelessWidget {
+class OnBoardingPage extends StatefulWidget {
   const OnBoardingPage({Key key}) : super(key: key);
+
+  @override
+  State<OnBoardingPage> createState() => _OnBoardingPageState();
+}
+
+class _OnBoardingPageState extends State<OnBoardingPage> {
+  final controller = PageController();
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
+        controller: controller,
         children: [
           Container(
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 290,
+                  ),
+                  Text(
+                    "مسافر للرياض وماتعرف احد؟",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.readexPro(
+                        textStyle:
+                            TextStyle(color: Colors.white, fontSize: 35)),
+                  ),
+                ],
+              ),
+            ),
             decoration: BoxDecoration(
               image: DecorationImage(
-                image: AssetImage("bg.png"),
+                image: AssetImage("assets/wel2.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 290,
+                  ),
+                  Text(
+                    "ودك في شخص يعلمك الأماكن السرية؟",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.readexPro(
+                        textStyle:
+                            TextStyle(color: Colors.white, fontSize: 35)),
+                  ),
+                ],
+              ),
+            ),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/wel3.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 290,
+                  ),
+                  Text(
+                    "ايش فيه احسن من ولد الديرة يخاويك؟",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.readexPro(
+                        textStyle:
+                            TextStyle(color: Colors.white, fontSize: 35)),
+                  ),
+                ],
+              ),
+            ),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/wel4.jpg"),
                 fit: BoxFit.cover,
               ),
             ),
           )
         ],
+      ),
+      bottomSheet: Container(
+        decoration: BoxDecoration(
+          color: Colors.black,
+          border: Border.all(style: BorderStyle.solid, color: Colors.black),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 2),
+        height: 50,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              width: 60,
+            ),
+            Center(
+              child: SmoothPageIndicator(
+                controller: controller,
+                count: 3,
+                effect: SlideEffect(activeDotColor: Colors.white),
+              ),
+            ),
+            // Spacer(),
+            TextButton(
+                onPressed: () {},
+                child: Text(
+                  "تخطي",
+                  style: TextStyle(color: Colors.white),
+                ))
+          ],
+        ),
       ),
     );
   }
