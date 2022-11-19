00cd3b8bbd75581ec0118cb1a1842d7a05c4516a
YousefAl-Ahmed <yousef.alahmed12@gmail.com>
2022-11-19 13:24:58 +0300

    Register2 ready

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/login_view.dart';
+import 'package:flutter_application_1/screens/register2.dart';
 
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: LoginView(),
+    home: SecondRegisterPage(),
   ));
 }
