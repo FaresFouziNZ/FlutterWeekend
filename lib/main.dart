a9dfcc755e0c499e7ba61be1e7ad3df12fc2f8d8
FaresFouziNZ <74138397+FaresFouziNZ@users.noreply.github.com>
2022-11-19 12:23:04 +0300

    firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/screens/driver_details_view.dart';
+import 'package:flutter_application_1/screens/drivers_list_view.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: DriverDetailsView(),
+    home: DriversListView(),
   ));
 }
